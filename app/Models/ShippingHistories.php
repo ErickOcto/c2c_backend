14cf163d0f38adecb8c6d1e91062c8436a8720dd
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2026-04-02 12:37:51 +0700

    feat: scaffold models, controllers, migrations, factories, and seeders for cart, wishlist, shipping, and dispute modules

---
+++ app/Models/ShippingHistories.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShippingHistories extends Model
+{
+    /** @use HasFactory<\Database\Factories\ShippingHistoriesFactory> */
+    use HasFactory;
+}
---
+++ database/factories/ShippingHistoriesFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ShippingHistories;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ShippingHistories>
+ */
+class ShippingHistoriesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
